496628bc1b8e331960b35be32bbc679dbc0c0422
Uszczi <mateuszpapuga24@gmail.com>
2022-04-17 22:49:45 +0200

    :tada: add simple docker files

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from fastapi import FastAPI, Request, Response
 app = FastAPI()
 
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-
-    uvicorn.run("main:app", host="::", port=8000)
+    print("Heeelefalfae")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
