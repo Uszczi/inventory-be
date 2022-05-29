e65d3fe3d3a2aa08055342d7d9db06e89053f83e
Mateusz Papuga <45439812+Uszczi@users.noreply.github.com>
2022-05-29 21:24:22 +0200

    Automate deploy  (#2)

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def read_root():
 
 if __name__ == "__main__":
     print("Heeelefalfae")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8010)
