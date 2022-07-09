7e236434f51980f0c2a6eb4de480194f3f3341e7
Uszczi <mateuszpapuga24@gmail.com>
2022-07-09 18:04:19 +0200

    s

---
+++ src/inv/models/alembic/versions/9c3ab4a7f282_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9c3ab4a7f282
+Revises: e646c653147f
+Create Date: 2022-06-14 10:49:55.372682
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c3ab4a7f282'
+down_revision = 'e646c653147f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
